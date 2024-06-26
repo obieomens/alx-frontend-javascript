ad001ed2db89a9942805b00134dc5e4b18033f36
obie <obieomens@gmail.com>
2024-06-26 16:34:03 +0100

    updated

--- 0x01-ES6_promise/7-load_balancer.js
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -1,4 +1,4 @@
 export default function loadBalancer(chinaDownload, USDownload) {
-    const data = Promise.race([chinaDownload, USDownload]).then((value) => value);
-    return data;
-  }
+  const data = Promise.race([chinaDownload, USDownload]).then((value) => value);
+  return data;
+}
