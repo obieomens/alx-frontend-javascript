daf1af89854d206262906595d92649fe0ab19bbb
obie <obieomens@gmail.com>
2024-06-26 15:40:47 +0100

    updated lint

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,13 +1,12 @@
 export default function getFullResponseFromAPI(a) {
-    return new Promise((resolve, reject)=> {
-        if (a == true){
-            resolve({
-                status: 200,
-                body: 'Success',
-            })
-        }else if (a == false){
-            reject(Error('The fake API is not working currently'));
-        }
-    })
+  return new Promise((resolve, reject) => {
+    if (a === true) {
+      resolve({
+        status: 200,
+        body: 'Success',
+      });
+    } else if (a === false) {
+      reject(Error('The fake API is not working currently'));
+    }
+  });
 }
-
