9ee8a9409549ba2c6f298d1f05582aaae07e748a
obie <obieomens@gmail.com>
2024-06-27 13:02:09 +0100

    updated

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,5 +1,5 @@
 import ClassRoom from "./0-classroom.js"
 export default function initializeRooms() {
   const sizes = [19, 20, 34];
-    return sizes.map(size => new ClassRoom(size));
+  return sizes.map(size => new ClassRoom(size));
 }
