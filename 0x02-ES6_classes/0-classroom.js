3b48788b1f2b227650263494c4b148a3b52fabac
obie <obieomens@gmail.com>
2024-06-27 11:32:39 +0100

    updated

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,6 @@
+export default class ClassRoom {
+  constructor(_maxStudentsSize ){
+    this._maxStudentsSize = _maxStudentsSize;
+   }
+
+}
---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,5 @@
+import ClassRoom from "./0-classroom.js"
+export default function initializeRooms() {
+  const sizes = [19, 20, 34];
+    return sizes.map(size => new ClassRoom(size));
+}
