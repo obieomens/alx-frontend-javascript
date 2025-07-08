c3add78f4f8af3704a8e7979a7a300fb18f353a9
obie <obieomens@gmail.com>
2025-07-09 00:55:08 +0100

    add main.ts

---
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -0,0 +1,24 @@
+const cpp: Subjects.Cpp = new Subjects.Cpp();
+const java: Subjects.Java = new Subjects.Java();
+const react: Subjects.React = new Subjects.React();
+
+const cTeacher: Subjects.Teacher = {
+  firstName: 'John',
+  lastName: 'Doe',
+  experienceTeachingC: 10,
+};
+
+console.log('C++');
+cpp.setTeacher = cTeacher;
+console.log(cpp.getRequirements());
+console.log(cpp.getAvailableTeacher());
+
+console.log('Java');
+java.setTeacher = cTeacher;
+console.log(java.getRequirements());
+console.log(java.getAvailableTeacher());
+
+console.log('React');
+react.setTeacher = cTeacher;
+console.log(react.getRequirements());
+console.log(react.getAvailableTeacher());
