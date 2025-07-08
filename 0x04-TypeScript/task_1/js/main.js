0897aa0648227a74386a72a1f1f78e4a656c8207
obie <obieomens@gmail.com>
2025-07-08 10:42:39 +0100

    update

--- 0x04-TypeScript/task_1/js/main.js
+++ 0x04-TypeScript/task_1/js/main.js
@@ -6,3 +6,15 @@ var teacher3 = {
     contract: false,
 };
 console.log(teacher3);
+var director1 = {
+    firstName: 'John',
+    lastName: 'Doe',
+    location: 'London',
+    fullTimeEmployee: true,
+    numberOfReports: 17,
+};
+console.log(director1);
+var printTeacher = function (firstName, lastName) {
+    return "".concat(firstName.charAt(0), ". ").concat(lastName);
+};
+console.log(printTeacher('John', 'Doe'));
--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -7,6 +7,15 @@ location: string;
 [key: string]: any;
 }
 
+interface Directors extends Teacher {
+    numberOfReports: number;
+}
+ 
+interface printTeacherFunction {
+    (firstName: string, lastName: string): string;
+}
+
+
 const teacher3: Teacher = {
     firstName: 'John',
     lastName: 'Doe',
@@ -14,4 +23,23 @@ const teacher3: Teacher = {
     location: 'London',
     contract: false,
 };
+
+
 console.log(teacher3);
+
+const director1: Directors = {
+    firstName: 'John',
+    lastName: 'Doe',
+    location: 'London',
+    fullTimeEmployee: true,
+    numberOfReports: 17,
+};
+
+console.log(director1);
+
+const printTeacher: printTeacherFunction = (firstName, lastName) =>{
+    return `${firstName.charAt(0)}. ${lastName}`;
+};
+console.log(printTeacher('John', 'Doe'));
+    
+
